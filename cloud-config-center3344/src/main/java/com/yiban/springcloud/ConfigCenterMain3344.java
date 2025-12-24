c6351b4e8a6019ed3c0241fe8906f71005196997
weiwei.duan <weiwei.duan@linkflowtech.com>
2025-12-24 17:19:46 +0800

    Config(配置中心)的demo, first commit 1. config center 服务端demo

---
+++ cloud-config-center3344/src/main/java/com/yiban/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,22 @@
+package com.yiban.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author david.duan
+ * @packageName com.yiban.springcloud
+ * @className ConfigCenterMain3344
+ * @date 2025/12/24
+ * @description
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
